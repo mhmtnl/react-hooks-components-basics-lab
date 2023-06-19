9157eb2e933b5bfed149eb275b1b7238655aaad2
mehmetunlu <munlu4037@gmail.com>
2023-06-19 14:15:33 +0300

    Completed asssignment

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,14 @@ function NavBar() {
   );
 }
 
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
+
 function Home() {
   return (
     <div id="home">
@@ -16,15 +24,13 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
